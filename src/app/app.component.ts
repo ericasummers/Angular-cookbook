0a314b06cacd567c84c9f14edf726f699c62c711
Erica Wright and Felix Oporto Lopez <erica@>
2017-03-28 10:57:23 -0700

    add show/hide to recipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,5 +17,7 @@ export class AppComponent {
 }
 
 export class Recipe {
-  constructor(public title: string, public ingredients: string [], public directions: string []) { }
+  constructor(public title: string, public ingredients: string [], public directions: string []) {
+  }
+
 }
